9ca7a01ca506d23e4b30783efd363f97c49a7f63
陆秋石 <uicestone@gmail.com>
2013-09-23 10:06:15 +0800

    fix: 首页文章列表显示不全

--- wp-content/themes/lawsuit/section-headline.php
+++ wp-content/themes/lawsuit/section-headline.php
@@ -2,13 +2,7 @@
 		<div class="wrapper">
 			<div class="widget column-3-1 news">
 				<h2><a href="/tag/团队动态/" target="_blank">团队动态</a></h2>
-				<!--
-				<hr />
 				<?query_posts(array('tag'=>'团队动态'));?>
-				<?the_post();?>
-				<?the_post_thumbnail(array(283,118))?>
-				<h3><?the_title()?></h3>
-				-->
 				<ul>
 					<?while(have_posts()):the_post();?>
 					<li><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
@@ -18,13 +12,7 @@
 			</div>
 			<div class="widget column-3-1 news">
 				<h2><a href="/tag/律师观点/" target="_blank">律师观点</a></h2>
-				<!--
-				<hr />
 				<?query_posts(array('tag'=>'律师观点'));?>
-				<?the_post();?>
-				<?the_post_thumbnail(array(283,118))?>
-				<h3><?the_title()?></h3>
-				-->
 				<ul>
 					<?while(have_posts()):the_post();?>
 					<li><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
@@ -34,13 +22,7 @@
 			</div>
 			<div class="widget column-3-1 news">
 				<h2><a href="/tag/案例分析/" target="_blank">案例分析</a></h2>
-				<!--
-				<hr />
-				<?query_posts(array('tag'=>'案例分析','post_per_page'=>1));?>
-				<?the_post();?>
-				<?the_post_thumbnail('headline')?>
-				<h3><?the_title()?></h3>
-				-->
+				<?query_posts(array('tag'=>'案例分析'));?>
 				<ul>
 					<?while(have_posts()):the_post();?>
 					<li><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
